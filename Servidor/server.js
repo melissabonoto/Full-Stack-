69119808810aaad5e3c47da704d6cbf6020ccc84
melissabonoto <bonotomelissa@gmail.com>
2026-04-26 01:03:30 -0300

    servidor + organização pastas

--- Servidor/server.js
+++ Servidor/server.js
@@ -1,12 +1,10 @@
-require("colors");
 var http = require("http");
 var express = require("express");
 
 var app = express();
 
-app.use(express.static("./public"));
+app.use(express.static('./public'));
+var server = http.createServer (app);
 
-var server = http.createServer(app);
 server.listen(80);
-
-console.log("Servidor Rodando...".rainbow)
+console.log("Servidor rodando")
