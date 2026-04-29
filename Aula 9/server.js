627427fcbef2064955d1bd5f45d2a0d7169d5ff3
melissabonoto <bonotomelissa@gmail.com>
2026-04-29 16:57:18 -0300

    aula 9

--- Aula 9/server.js
+++ Aula 9/server.js
@@ -1,12 +1,53 @@
 require("colors");
 var http = require("http");
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
-
 app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
 var server = http.createServer(app);
 server.listen(80);
 
 console.log("Servidor Rodando...".rainbow)
+
+app.get("/", function(req, res){
+    res.redirect("home.html")
+})
+
+app.get("/inicio", function (req, res){
+    var text = req.query.text;
+    var number = req.query.number;
+    var color = req.query.color;
+    console.log(text, number, color);
+    console.log("requisição feita por GET");
+})
+
+app.post("/inicio", function(req, res){
+    var text = req.body.text;
+    var number = req.body.number;
+    var color = req.body.color;
+    console.log(text, number, color);
+    console.log("requisição feita por POST");
+});
+
+app.post("/cadastro", function(req, res){
+    var nome = req.body.nome;
+    var login = req.body.login;
+    var senha = req.body.senha;
+
+    console.log(nome, login, senha);
+
+    res.render("resposta.ejs", 
+        {resposta: "Usuário cadastrado com sucesso!"})
+});
+
+app.get("/for", function(req, res){
+    var qtde = req.query.qtde
+    res.render("exemplofor.ejs", {qtde})
+
+});
