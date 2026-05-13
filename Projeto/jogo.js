e78ff4ec49f1a74a8873be597442c81a6c7302c6
melissabonoto <bonotomelissa@gmail.com>
2026-05-13 15:14:30 -0300

    aula

--- Projeto/jogo.js
+++ Projeto/jogo.js
@@ -1,15 +1,61 @@
+// var canvas = document.getElementById("canvas");
+// var ctx = canvas.getContext("2d");
+
+// let player = {
+//     x: 100,
+//     y: 250,
+//     w: 150,
+//     h: 150,
+//     img: new Image()
+// }
+
+// player.img.src = "imagens/player_s.png";
+
+// player.img.onload = function () {
+//     ctx.drawImage(player.img, player.x, player.y, player.w, player.h);
+// }
+
+// document.addEventListener("keydown", function(evento){
+//     var tecla = evento.key;
+//     console.log(tecla);
+
+// var vel = 20;
+
+//     if (tecla == "ArrowUp") {player.y -= vel}
+//     if (tecla == "ArrowDown") {player.y += vel}
+//     if (tecla == "ArrowLeft") {player.x -= vel}
+//     if (tecla == "ArrowRight") {player.x += vel}
+
+//     if (player.y >= canvas.height - player.h) {player.y = canvas.height - player.h}
+//     if (player.y <= 0) {player.y = 0}
+//     if (player.x >= canvas.width - player.w) {player.x = canvas.width - player.w}
+//     if (player.x <= 0) {player.x = 0}
+    
+
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.drawImage(player.img, player.x, player.y, player.w, player.h);
+// })
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+// var fundo = new Image ();
+// fundo.src = "fundo.jpg";
+
+// fundo.onload = function () {
+//     ctx.drawImage(fundo, 0, 0, canvas.width, canvas.height);
+// };
+
 let player = {
-    x: 100,
-    y: 250,
-    w: 150,
-    h: 150,
+    x: 550,
+    y: 300,
+    w: 60,
+    h: 60,
+    tamanho: 2,
     img: new Image()
 }
 
-player.img.src = "imagens/player_s.png";
+player.img.src = "playerr.png";
 
 player.img.onload = function () {
     ctx.drawImage(player.img, player.x, player.y, player.w, player.h);
@@ -19,12 +65,24 @@ document.addEventListener("keydown", function(evento){
     var tecla = evento.key;
     console.log(tecla);
 
-var vel = 20;
+if (player.tamanho < 30){
+    var vel = 50;
+}
+else if (player.tamanho < 60 ){
+    var vel = 40;
+}
+
+else {
+    var vel = 30;
+}
 
     if (tecla == "ArrowUp") {player.y -= vel}
     if (tecla == "ArrowDown") {player.y += vel}
     if (tecla == "ArrowLeft") {player.x -= vel}
     if (tecla == "ArrowRight") {player.x += vel}
+
+    if (tecla == "ArrowLeft") {player.img.src = "playerl.png"}
+    if (tecla == "ArrowRight") {player.img.src = "playerr.png"}
 
     if (player.y >= canvas.height - player.h) {player.y = canvas.height - player.h}
     if (player.y <= 0) {player.y = 0}
@@ -36,3 +94,110 @@ var vel = 20;
     ctx.drawImage(player.img, player.x, player.y, player.w, player.h);
 })
 
+let inimigos_d = [];
+    function criarInimigoDireita(){
+            let tamanho = Math.random() * 60 + 30;
+
+            let inimigo = {
+                x: canvas.width,
+                y: Math.random() * (canvas.height - tamanho),
+
+                tamanho: tamanho,
+
+                w: tamanho,
+                h: tamanho,
+
+                mov: Math.random() * 4 + 1,
+
+                img: new Image()
+
+        };
+        if (tamanho < 55){
+            inimigo.img.src = `peixe10.png`;
+        }
+        else if  (tamanho < 80) {
+            inimigo.img.src = `peixe5.png`;
+            inimigo.w += 80;
+            inimigo.h += 80;
+        }
+        else {
+            inimigo.img.src = `peixe2.png`;
+            inimigo.w += 150;
+            inimigo.h += 150;
+        }
+
+        inimigos_d.push(inimigo);
+
+    }
+
+    let inimigos_e = [];
+    function criarInimigoEsquerda(){
+            let tamanho = Math.random() * 60 + 30;
+
+            let inimigo = {
+                x: 0,
+                y: Math.random() * (canvas.height - tamanho),
+
+                tamanho: tamanho,
+
+                w: 10 + tamanho,
+                h: 10 + tamanho,
+
+                mov: Math.random() * 4 + 1,
+
+                img: new Image()
+
+        };
+        if (tamanho < 50){
+            inimigo.img.src = `peixe-7.png`;
+        }
+        else if  (tamanho < 80) {
+            inimigo.img.src = `peixe-4.png`;
+            inimigo.w += 80;
+            inimigo.h += 80;
+        }
+        else {
+            inimigo.img.src = `peixe-6.png`;
+            inimigo.w += 90;
+            inimigo.h += 90;
+        }
+
+        inimigos_e.push(inimigo);
+    }
+
+    setInterval(criarInimigoDireita, 3000);
+    setInterval(criarInimigoEsquerda, 4000);
+
+let vida = 3;
+function desenhar(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i=0; i <inimigos_d.length; i++){
+        let inimigo = inimigos_d[i];
+
+        ctx.drawImage(inimigo.img, inimigo.x, inimigo.y, inimigo.w, inimigo.h);
+        inimigo.x = inimigo.x - inimigo.mov
+    }
+
+     for (let i=0; i <inimigos_e.length; i++){
+        let inimigo = inimigos_e[i];
+
+        ctx.drawImage(inimigo.img, inimigo.x, inimigo.y, inimigo.w, inimigo.h);
+        inimigo.x = inimigo.x + inimigo.mov
+    }
+    
+    ctx.drawImage(player.img, player.x, player.y, player.w, player.h)
+
+    ctx.font = '30px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText('Vidas: ', 50, 50);
+    ctx.fillText(vida, 150, 50);
+
+     ctx.fillText('Tamanho: ', 550, 50);
+     ctx.fillText(player.tamanho, 700, 50);
+
+
+    requestAnimationFrame(desenhar)
+}
+
+desenhar();
